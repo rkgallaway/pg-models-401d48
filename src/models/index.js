09fe394e12824a68d47d426ebf1ffb6425a1fbd2
Ryan Gallaway <rkgallaway@msn.com>
2022-09-22 16:01:08 -0700

    complete project

--- src/models/index.js
+++ src/models/index.js
@@ -14,7 +14,14 @@ const DATABASE_URL = process.env.NODE_ENV === 'test'
   : process.env.DATABASE_URL;
 
 // instantiates our database
-const sequelizeDatabase = new Sequelize(DATABASE_URL);
+const sequelizeDatabase = new Sequelize(DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
 //create CustomersModel with our Schema
 const CustomersModel = customersSchema(sequelizeDatabase, DataTypes);
